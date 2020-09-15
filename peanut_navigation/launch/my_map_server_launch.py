08acfa2b3f23e2516cbfa87db0dc0586ecb0e23b
Mrunal <mrunaljsarvaiya@gmail.com>
2020-09-14 17:41:15 -0700

    Add custom launch file and nav params

---
+++ peanut_navigation/launch/my_map_server_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="nav2_map_server",
+            executable="map_server",
+            name="my_map_server",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                {"yaml_filename": "/home/peanut/peanut2_ws/src/peanut_ros2/peanut_navigation/maps/office.yaml"}
+            ]
+        )
+    ])
