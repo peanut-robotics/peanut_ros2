05653c35a9beb1462636c3c375c4b957b420b136
mrunaljsarvaiya <mrunaljsarvaiya@gmail.com>
2020-10-12 16:27:31 -0700

    Update bash files

--- docker/ros1-bridge/source_bridge.sh
+++ docker/ros1-bridge/source_bridge.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+source /opt/ros/noetic/setup.bash 
+source /opt/ros/foxy.setup.bash 
+source ~/peanut2_ws/install/setup.bash
+
 source ~/bridge_ws/install/setup.bash
--- docker/ros1-bridge/source_peanut2.sh
+++ docker/ros1-bridge/source_peanut2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+source /opt/ros/foxy/setup.bash
+
 source ~/peanut2_ws/install/setup.bash
