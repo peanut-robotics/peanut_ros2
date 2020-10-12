46e1e0f877a2f4b6e40aeeb4240f985070a26c25
Mrunal <mrunaljsarvaiya@gmail.com>
2020-10-12 16:39:28 -0700

    Update maps names

--- peanut2_navigation/launch/my_map_server_launch.py
+++ peanut2_navigation/launch/my_map_server_launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             output="screen",
             emulate_tty=True,
             parameters=[
-                {"yaml_filename": "/home/peanut/peanut2_ws/src/peanut_ros2/peanut_navigation/maps/office.yaml"}
+                {"yaml_filename": "/home/peanut/peanut2_ws/src/peanut_ros2/peanut2_navigation/maps/office.yaml"}
             ]
         )
     ])
